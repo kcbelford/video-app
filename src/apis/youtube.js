d11a6e8af38bbbe8e9cd3c3f1bd55a72e6ab4213
kcbelford <kcbelford@hotmail.com>
2020-10-20 21:18:45 -0700

    display video title and thumbnail

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyBT6tV5lhGCIR00KeGAq93wOD0gRs1ELTE';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    type: 'video',
+    maxResults: 5,
+    key: KEY
+  }
+});
